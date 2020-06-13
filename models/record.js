55538b44755edf6c4b54141c9c20be989e4a993b
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-06-14 00:04:37 +0800

    feat: create user and record schemas

---
+++ models/record.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const recordSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  retailer: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Record', recordSchema)
+
